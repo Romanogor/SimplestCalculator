ea247941d8273b78172acebc9169858514580fbb
Roman Ohorodnyk <RomanO@intellis.io>
2019-12-26 17:22:10 +0200

    Added Moq to tests

--- SimplestCalculator.UnitTests/CalculatorTests.cs
+++ SimplestCalculator.UnitTests/CalculatorTests.cs
@@ -8,15 +8,26 @@ namespace SimplestCalculator.UnitTests
     [TestFixture]
     class CalculatorTests
     {
+
+        private Moq.Mock<ILoggerManager> _mockLogger = null;
+        private Calculator _calculator = null;
+            
+            [SetUp]
+        public void Setup()
+        {
+            _mockLogger = new Moq.Mock<ILoggerManager>();
+            _mockLogger.Setup(x => x.LogMessage("testMessage"));
+            _calculator = new Calculator(_mockLogger.Object);
+        }
+
         [Test]
         public void Add_GoodParameters_ReturnSum()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
+            
 
             //Act
-            var result = calculator.Add("4", "7");
+            var result = _calculator.Add("4", "7");
 
             //Assert
             Assert.True(result == 11);
@@ -28,11 +39,9 @@ namespace SimplestCalculator.UnitTests
         public void Add_InValidFirstParameter_ReturnNull()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
-
+            
             //Act
-            var result = calculator.Add("sdf", "2");
+            var result = _calculator.Add("sdf", "2");
 
             //Assert
             Assert.True(result == null);
@@ -43,45 +52,34 @@ namespace SimplestCalculator.UnitTests
         public void Add_InValidSecondParameter_ThrowException()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
-
+            
             //Act
             var ex = Assert.Catch<Exception>(
-                () => calculator.Add("3", "ase"));
+                () => _calculator.Add("3", "ase"));
 
             //Assert
             StringAssert.Contains("Input string was ", ex.Message);
-
-
-
         }
 
         [Test]
         public void Substruct_GoodParameters_ReturnSubstructResult()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
-
+            
             //Act
-            var result = calculator.Substruct("44", "27");
+            var result = _calculator.Substruct("44", "27");
 
             //Assert
             Assert.True(result == 17);
         }
 
-
-
         [Test]
         public void Substruct_InValidFirstParameter_ReturnNull()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
 
             //Act
-            var result = calculator.Substruct("sdf", "4");
+            var result = _calculator.Substruct("sdf", "4");
 
             //Assert
             Assert.True(result == null);
@@ -92,45 +90,34 @@ namespace SimplestCalculator.UnitTests
         public void Substruct_InValidSecondParameter_ThrowException()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
 
             //Act
             var ex = Assert.Catch<Exception>(
-                () => calculator.Substruct("3", "ase"));
+                () => _calculator.Substruct("3", "ase"));
 
             //Assert
             StringAssert.Contains("Input string was ", ex.Message);
-
-
-
         }
 
         [Test]
         public void Multiply_GoodParameters_ReturnMultiplyResult()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
 
             //Act
-            var result = calculator.Multiply("4", "7");
+            var result = _calculator.Multiply("4", "7");
 
             //Assert
             Assert.True(result == 28);
         }
 
-
-
         [Test]
         public void Multiply_InValidFirstParameter_ReturnNull()
         {
-            //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
+            //Arrange            
 
             //Act
-            var result = calculator.Multiply("sdf", "4");
+            var result = _calculator.Multiply("sdf", "4");
 
             //Assert
             Assert.True(result == null);
@@ -141,12 +128,10 @@ namespace SimplestCalculator.UnitTests
         public void Multiply_InValidSecondParameter_ThrowException()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
 
             //Act
             var ex = Assert.Catch<Exception>(
-                () => calculator.Multiply("3", "ase"));
+                () => _calculator.Multiply("3", "ase"));
 
             //Assert
             StringAssert.Contains("Input string was ", ex.Message);
@@ -159,11 +144,9 @@ namespace SimplestCalculator.UnitTests
         public void Divide_GoodParameters_ReturnDivideResult()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
 
             //Act
-            var result = calculator.Divide("8", "2");
+            var result = _calculator.Divide("8", "2");
 
             //Assert
             Assert.True(result == 4);
@@ -175,11 +158,9 @@ namespace SimplestCalculator.UnitTests
         public void Divide_InValidFirstParameter_ReturnNull()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
 
             //Act
-            var result = calculator.Divide("sdf", "4");
+            var result = _calculator.Divide("sdf", "4");
 
             //Assert
             Assert.True(result == null);
@@ -190,36 +171,26 @@ namespace SimplestCalculator.UnitTests
         public void Divide_InValidSecondParameter_ThrowException()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
 
             //Act
             var ex = Assert.Catch<Exception>(
-                () => calculator.Divide("3", "ase"));
+                () => _calculator.Divide("3", "ase"));
 
             //Assert
             StringAssert.Contains("Input string was ", ex.Message);
-
-
-
         }
 
         [Test]
         public void Divide_SecondParameterIsZero_ThrowDivideByZeroException()
         {
             //Arrange
-            ILoggerManager loggerManager = new FakeLoggerManager();
-            Calculator calculator = new Calculator(loggerManager);
 
             //Act
             var ex = Assert.Catch<DivideByZeroException>(
-                () => calculator.Divide("3", "0"));
+                () => _calculator.Divide("3", "0"));
 
             //Assert
             StringAssert.Contains("divide by zero", ex.Message);
-
-
-
         }
     }
 
